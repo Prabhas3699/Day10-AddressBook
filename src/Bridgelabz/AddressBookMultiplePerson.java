c5bbc7463bb7f2a457cefded3e99862e7a91da1f
Prabhas3699 <prabhak620@gmail.com>
2022-08-27 17:29:10 +0530

    Program for address book to add multiple  contact

---
+++ src/Bridgelabz/AddressBookMultiplePerson.java
@@ -0,0 +1,55 @@
+package Bridgelabz;
+
+import java.util.Scanner;
+
+public class AddressBookMultiplePerson {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	ContactMultiple AddressBook = new ContactMultiple();
+	System.out.println("Enter the number of person:");
+	int person =sc.nextInt();
+	
+	for(int i=1; i<=person;i++) {
+		System.out.println("Enter the \'"+i+"\' person detail:");
+	/*
+	 * Calling createContact method
+	 */
+	AddressBook.createContact();
+	/*
+	 * Calling displayContacts method
+	 */
+	AddressBook.displayContacts();	
+	System.out.println("-------------------------------------------");
+	}
+	
+	System.out.println("Press \'1\' to edit contact Or Press \'2\' to delete contact Or Press \\'3\\' to Display contact");
+	System.out.println("Enter the option:");
+	int option = sc.nextInt();
+	
+	if(option == 1) {
+	/*
+	 * Calling editContact method
+	 */
+	AddressBook.editContact();
+	/*
+	 * Calling displayContacts method
+	 */
+	System.out.println("-------------------------------------------");
+	AddressBook.displayContacts();
+	}else if(option == 2){
+	/*
+	 * Calling to deleteContact method
+	 */
+	AddressBook.deleteContact();
+	/*
+	 * Calling displayContacts method
+	 */
+	System.out.println("-------------------------------------------");
+	AddressBook.displayContacts();
+	}else if (option ==3) {
+		System.out.println("-------------------------------------------");
+		AddressBook.displayContacts();
+	}
+	}
+}

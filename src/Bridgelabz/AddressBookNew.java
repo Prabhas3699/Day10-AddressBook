86c560f77ea223db0a4f433d43955764b2d6bc39
Prabhas3699 <prabhak620@gmail.com>
2022-08-27 16:57:48 +0530

    Program for address book to add contact from user

---
+++ src/Bridgelabz/AddressBookNew.java
@@ -0,0 +1,16 @@
+package Bridgelabz;
+
+public class AddressBookNew {
+	public static void main(String[] args) {
+		
+		ContactNew AddressBook = new ContactNew();
+		/*
+		 * Calling createContact method
+		 */
+		AddressBook.createContact();
+		/*
+		 * Calling displayContacts method
+		 */
+		AddressBook.displayContacts();	
+	}
+}

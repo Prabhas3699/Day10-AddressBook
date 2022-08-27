05bc5ccb71efbe146c01133972c36d194899dc07
Prabhas3699 <prabhak620@gmail.com>
2022-08-27 17:21:56 +0530

    Program for address book to Edit contact

---
+++ src/Bridgelabz/AddressBookEdit.java
@@ -0,0 +1,28 @@
+package Bridgelabz;
+
+public class AddressBookEdit {
+	public static void main(String[] args) {
+		
+		ContactEdit AddressBook = new ContactEdit();	
+		/*
+		 * Calling createContact method
+		 */
+		AddressBook.createContact();
+		/*
+		 * Calling displayContacts method
+		 */
+		AddressBook.displayContacts();	
+
+		System.out.println("\nPress Enter to edit contact:");
+		/*
+		 * Calling editContact method
+		 */
+		AddressBook.editContact();
+		System.out.println("\nAdressbook of the person");
+		/*
+		 * Calling displayContacts method
+		 */
+		AddressBook.displayContacts();
+		
+	}
+}
